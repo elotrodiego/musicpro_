ad0c1668629b7f62b8bd317786d7dd2a9f1683e8
elotrodiego <elotrodiegoluna@gmail.com>
2022-05-10 12:43:26 -0400

    aaaaaaaaaaaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require ('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myConnection');
 
 // settings
 app.set('port', 3000);
@@ -9,7 +12,14 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // middlewares
-
+app.use(morgan('dev'));
+app.use(myConnection(mysql, { 
+    host: 'localhost',
+    user: 'root',
+    password: 'pass',
+    port: 3306,
+    database: 'musicpro'
+    }, 'single')); 
 // routes
 app.use(require('./routes/index'));
 // static files
